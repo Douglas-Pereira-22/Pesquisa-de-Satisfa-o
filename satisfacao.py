c7ce96e7bc71af9c270bc61b8bc36ba328989c47
Douglas Pereira <sousa_douglas@live.com>
2026-04-09 20:53:39 -0300

    Update satisfacao.py

--- satisfacao.py
+++ satisfacao.py
@@ -1,14 +1,16 @@
-n1 =  0
+#Variaveis
+n1 =  0 
 n2 =  0
 n3 =  0
-
-for i in range(3):
+#Pedindo ao entrevistado seu nome, idade e sua opinião por 50 vezes utilizando o For.
+for i in range(50):
     print('Queremos saber sua opinião sobre o nosso atendimento')
     nome = str(input('Digite seu nome: '))
     idade = int(input('Digite sua idade: '))
     opiniao = int((input('Digite 1-Excelente 2-Bom 3-Ruim: ')))
     print('Obrigado por participar!')
     print('*'*30)
+    #Fazendo a leitura e armazenamento correto da resposta utilizando o if, elif e else.
     if opiniao == 1:
         n1 += 1
     elif opiniao == 2:
@@ -18,6 +20,7 @@ for i in range(3):
     else:
         pass
 print('*'*30)
+#Mostrando o resultado da pesquisa.
 print(f'Execelente = {n1}')
 #print(f'Bom = {n2}')
 print(f'Ruim = {n3}')
